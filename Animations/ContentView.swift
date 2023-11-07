910cbdfc7577e9f8f564e9efeb29df787ad84cd7
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-11-07 13:36:23 +0300

    Customize animations.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -19,9 +19,19 @@ struct ContentView: View {
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .animation(.default, value: animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount)
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
